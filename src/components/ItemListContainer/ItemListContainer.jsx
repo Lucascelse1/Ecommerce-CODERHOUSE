36f5b96427f147ce4758b72a7a33bbfb186003ba
Lucas Celse <lucas.j.celse@gmail.come>
2024-02-26 02:49:40 -0300

    Ultimos cambios

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 const ItemListContainer = (props) => {
     return (
         <div className="w-full my-4">
-            <h1 className="text-center text-white text-4xl"> {props.greeting} </h1>
+            <h1 className="text-center text-white text-4xl font-body"> {props.greeting} </h1>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'body': ["Protest Guerrilla",],
+    },
   },
   plugins: [],
 }
