9775b2acfdacaccd54f259f30322bab7550db9e2
Lucas Celse <lucas.j.celse@gmail.come>
2024-04-09 20:30:10 -0300

    fixeando

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 import ItemList from "../ItemList/ItemList"
 import { useParams } from "react-router-dom"
 import { useNotification } from "../../notification/hooks/useNotification"
-
 import { getDocs, collection, query, where } from "firebase/firestore"
 import { db } from "../../services/firebase/firebaseConfig"
 
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,16 +1,46 @@
 import classes from './Navbar.module.css'
 import CartWidget from '../CartWidget/CartWidget'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import guns from './assets/guns.svg'
+import { useEffect, useState } from 'react'
+import { collection, getDocs, orderBy, query } from 'firebase/firestore'
+import { db } from '../../services/firebase/firebaseConfig'
 
 const Navbar = () => {
+
+    const [categories, setCategories] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const categoriesCollection = query(collection(db, 'categories'), orderBy('order'))
+
+        getDocs(categoriesCollection)
+            .then(QuerySnapshot => {
+                const categoriesAdapted = QuerySnapshot.docs.map(doc => {
+                    const data = doc.data()
+                    return {id: doc.id, ...data}
+                })
+                setCategories(categoriesAdapted)
+            })
+            .catch (error => {
+                console.error(error)
+            })
+    }, [])
+
     return (
         <header className={classes.header}>
-            <Link className='flex flex-col justify-center items-center text-white text-lg' to='/'>La Pirinola<img className='w-14 flex' src={guns} alt=""/></Link>
+            <h1 className='flex flex-col justify-center items-center text-white text-lg' onClick={() => navigate('/')}>La Pirinola<img className='w-14 flex' src={guns} alt=""/></h1>
             <nav className="bg-[#51616d] w-9/12 md:w-2/4 p-2 flex justify-around my-2 rounded-lg text-white">
-                <Link to='/category/Pistola' className='w-1/3 flex justify-center tracking-[.3rem]'>PISTOLAS</Link>
+
+                {
+                    categories.map(cat => {
+                        return <Link className='w-1/3 flex justify-center tracking-[.3rem]' key={cat.id} to={`/category/${cat.slug}`}>{cat.name}</Link>
+                    })
+                }
+
+                {/* <Link to='/category/Pistola' className='w-1/3 flex justify-center tracking-[.3rem]'>PISTOLAS</Link>
                 <Link to='/category/Fusiles' className='w-1/3 flex justify-center tracking-[.3rem]'>RIFLES</Link>
-                <Link to='/category/Cuchillos' className='w-1/3 flex justify-center tracking-[.3rem]'>CUCHILLOS</Link>    
+                <Link to='/category/Cuchillos' className='w-1/3 flex justify-center tracking-[.3rem]'>CUCHILLOS</Link> */}    
             </nav>
             <CartWidget />
         </header>
